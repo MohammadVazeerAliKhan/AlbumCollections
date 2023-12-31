34add87c49412bca730a87a84aefa96d74f86a08
Vazeer Khan <vazeerkhan511@gmail.com>
2023-12-31 13:10:34 +0530

    Update popup styling

--- src/Components/UpdateAlbum.jsx
+++ src/Components/UpdateAlbum.jsx
@@ -1,15 +1,31 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { MyContext } from "../AlbumsContext";
 
 const UpdateAlbum = ({ album, update }) => {
+  const [isOpen, setIsOpen] = useState(true);
   const [albumId, setAlbumId] = useState(album.id);
   const [userId, setUserId] = useState(album.userId);
   const [albumTitle, setAlbumTitle] = useState(album.title);
   const { albums, setAlbums } = useContext(MyContext);
-
   const handleInputChange = (e, setter) => {
     setter(e.target.value);
   };
+  useEffect(() => {
+    const body = document.body;
+
+    if (isOpen) {
+      // Lock scroll when the popup is open
+      body.style.overflow = "hidden";
+    } else {
+      // Enable scroll when the popup is closed
+      body.style.overflow = "auto";
+    }
+
+    // Cleanup: Restore scroll on component unmount
+    return () => {
+      body.style.overflow = "auto";
+    };
+  }, [isOpen]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -43,8 +59,8 @@ const UpdateAlbum = ({ album, update }) => {
       setAlbumId("");
       setUserId("");
       setAlbumTitle("");
-
       alert("Album updated successfully!");
+      setIsOpen(false);
     } catch (error) {
       console.error("Error adding album:", error);
     }
@@ -52,62 +68,64 @@ const UpdateAlbum = ({ album, update }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="formAlbum">
-      <h2>Update the Album</h2>
-      <br />
-      <label>
-        Album ID:
-        <input
-          type="text"
-          value={albumId}
-          placeholder="Enter Album Id"
-          onChange={(e) => handleInputChange(e, setAlbumId)}
-          required
-          disabled
-        />
-      </label>
-      <br />
-      <label>
-        User ID:
-        <input
-          type="text"
-          value={userId}
-          placeholder="Enter User Id"
-          onChange={(e) => handleInputChange(e, setUserId)}
-          required
-          disabled
-        />
-      </label>
-      <br />
-      <label>
-        Album Title:
-        <input
-          type="text"
-          value={albumTitle}
-          placeholder="Enter Album Title"
-          onChange={(e) => handleInputChange(e, setAlbumTitle)}
-          required
-        />
-      </label>
-      <br />
-      <p>*Note: You cannot update userId and ID of the Album </p>
-      <br />
-      <div className="card-btns">
-        <button
-          className="btn red"
-          type="reset"
-          onClick={(e) => {
-            e.preventDefault();
-            update(false);
-          }}
-        >
-          Discard
-        </button>
-        <button className="btn green" type="submit">
-          Update
-        </button>
-      </div>
-    </form>
+    <div className="updatebg">
+      <form onSubmit={handleSubmit} className="formAlbum fixed ">
+        <h2>Update the Album</h2>
+        <br />
+        <label>
+          Album ID:
+          <input
+            type="text"
+            value={albumId}
+            placeholder="Enter Album Id"
+            onChange={(e) => handleInputChange(e, setAlbumId)}
+            required
+            disabled
+          />
+        </label>
+        <br />
+        <label>
+          User ID:
+          <input
+            type="text"
+            value={userId}
+            placeholder="Enter User Id"
+            onChange={(e) => handleInputChange(e, setUserId)}
+            required
+            disabled
+          />
+        </label>
+        <br />
+        <label>
+          Album Title:
+          <input
+            type="text"
+            value={albumTitle}
+            placeholder="Enter Album Title"
+            onChange={(e) => handleInputChange(e, setAlbumTitle)}
+            required
+          />
+        </label>
+        <br />
+        <p>*Note: You cannot update userId and ID of the Album </p>
+        <br />
+        <div className="card-btns">
+          <button
+            className="btn red"
+            type="reset"
+            onClick={(e) => {
+              e.preventDefault();
+              update(false);
+            }}
+          >
+            Discard
+          </button>
+          <button className="btn green" type="submit">
+            Update
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
